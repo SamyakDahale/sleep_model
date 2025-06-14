9da80ba61ee1637bb90d69ce0103396d6941aae3
SamyakDahale <samyakdahale123@gmail.com>
2025-06-14 11:01:15 +0530

    Upload1

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def predict():
     return jsonify({'result': predicted_label[0]})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
