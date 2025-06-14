6a231342d39bb379cc1db2e052175045a2123d56
SamyakDahale <samyakdahale123@gmail.com>
2025-06-14 11:33:16 +0530

    Upload6

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import joblib
 from flask import Flask, request, jsonify
 import numpy as np
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 # Load model and encoder
 model = joblib.load("rf_model.pkl")
